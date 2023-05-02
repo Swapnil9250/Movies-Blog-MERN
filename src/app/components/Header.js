d22c1f1410f0e431965d4723665512eef2946720
Swapnil9250 <surawarswapnil@gmail.com>
2023-05-02 16:19:23 +0530

    fix issue

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
     <header className={styles.main_header}>
         <div className={styles.navbar_brand}>|
         <Link href="/">
-          <Image src="/swapniltechk.png" alt="My logo" width={150} height={40} />
+          <Image src="/swapniltechk.png" alt="My logos" width={150} height={40} />
         </Link>
         </div>
         <Nav />
